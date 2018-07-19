270c86a9ded1c564cd30ffe08a61e4ada4177688
Sean Lin <seanlin1016@gmail.com>
2018-07-19 15:27:39 +0800

    Sign In Firebase by Facebook Account

--- FirebaseSocialLogin/ViewController.swift
+++ FirebaseSocialLogin/ViewController.swift
@@ -8,22 +8,61 @@
 
 import UIKit
 import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let facebookLoginButtton: FBSDKLoginButton = {
         let loginButton = FBSDKLoginButton()
         loginButton.translatesAutoresizingMaskIntoConstraints = false
+        for const in loginButton.constraints{
+            if const.firstAttribute == NSLayoutAttribute.height && const.constant == 28{
+                loginButton.removeConstraint(const)
+            }
+        }
+        loginButton.readPermissions = ["email", "public_profile"]
+
+        return loginButton
+    }()
+    
+    let customFBLoginButton: UIButton = {
+        let loginButton = UIButton(type: .system)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.setTitle("Facebook帳戶登入", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.backgroundColor = UIColor(red: 66/255, green: 103/255, blue: 178/255, alpha: 1)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        loginButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
+        return loginButton
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        view.addSubview(loginButton)
-        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        loginButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
-        loginButton.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        view.addSubview(facebookLoginButtton)
+        facebookLoginButtton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        facebookLoginButtton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        facebookLoginButtton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
+        facebookLoginButtton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        loginButton.delegate = self
+        facebookLoginButtton.delegate = self
+
+        view.addSubview(customFBLoginButton)
+        customFBLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        customFBLoginButton.topAnchor.constraint(equalTo: facebookLoginButtton.bottomAnchor, constant: 16).isActive = true
+        customFBLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
+        customFBLoginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+    }
+    
+    @objc private func handleCustomFBLogin(){
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil{
+                print("Custom FB Login failed: ", error ?? "error")
+            }
+            self.showFacebookEmailAdress()
+        }
+            
     }
     
     func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
@@ -32,11 +71,34 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             return
         }
         
-        print("Successfully logged in with facebook...")
+        showFacebookEmailAdress()
     }
     
     func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
         print("Did Log out of facebook")
+    }
+    
+    private func showFacebookEmailAdress(){
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else { return }
+        let credentials = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        Auth.auth().signInAndRetrieveData(with: credentials) { (result, error) in
+            if error != nil {
+                print("Failed to sign in Firebase by FB: ", error ?? "error")
+                return
+            }
+            print("Successfully logged in Firebase with facebook...")
+            print(result?.user.uid ?? "no uid")
+        }
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
+            if err != nil {
+                print("Failed to start graph request: ", err ?? "error")
+                return
+            }
+            print(result ?? "")
+        }
     }
     
 
